351d8214e903a9ab78c4011a2291cf793641e898
rena-jp <101486573+rena-jp@users.noreply.github.com>
2023-12-18 21:48:44 +0900

    Update 4.13.9

--- dist/hh-battle-simulator-v4.meta.js
+++ dist/hh-battle-simulator-v4.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hentai Heroes Battle Simulator v4
 // @namespace    https://github.com/rena-jp/hh-battle-simulator-v4
-// @version      4.13.8
+// @version      4.13.9
 // @description  Add a battle simulator to Hentai Heroes and related games
 // @author       rena
 // @match        https://*.hentaiheroes.com/*
