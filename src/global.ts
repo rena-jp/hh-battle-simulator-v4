565d60422dff4254df6f58809ccb9f3a59cf163b
rena-jp <101486573+rena-jp@users.noreply.github.com>
2025-06-11 22:57:29 +0900

    Add functions for external scripts

--- src/global.ts
+++ src/global.ts
@@ -20,6 +20,19 @@ window.HHBattleSimulator = {
         return await simulateFromFighters('Standard', playerFighter, opponentFighter);
     },
     /**
+     * @param {SimulationType} simType - 'FastChance' | 'FastPoints' | 'Chance' | 'Points' | 'Standard' | 'Full'
+     * @param {any} playerFighter - hero_data
+     * @param {any} opponentFighter - opponent_fighter.player
+     * @returns {Promise<SimulationResult<T>>} - results
+     */
+    async simulateFromFightersEx<T extends SimulationType>(
+        simType: T,
+        playerFighter: any,
+        opponentFighter: any,
+    ): Promise<SimulationResult<T>> {
+        return await simulateFromFighters(simType, playerFighter, opponentFighter);
+    },
+    /**
      * @param {any} playerTeam - hero_data.team
      * @param {any} opponentTeam - opponent_fighter.player.team
      * @param {number} mythicBoosterMultiplier - Default: 1, AME/LME/SME: 1.15, Headband: 1.25
@@ -38,5 +51,20 @@ window.HHBattleSimulator = {
         maxPoints: number;
     }> {
         return await simulateFromTeams('Standard', playerTeam, opponentTeam, mythicBoosterMultiplier);
+    },
+    /**
+     * @param {SimulationType} simType - 'FastChance' | 'FastPoints' | 'Chance' | 'Points' | 'Standard' | 'Full'
+     * @param {any} playerTeam - hero_data.team
+     * @param {any} opponentTeam - opponent_fighter.player.team
+     * @param {number} mythicBoosterMultiplier - Default: 1, AME/LME/SME: 1.15, Headband: 1.25
+     * @returns {Promise<SimulationResult<T>>} - results
+     */
+    async simulateFromTeamsEx<T extends SimulationType>(
+        simType: T,
+        playerTeam: any,
+        opponentTeam: any,
+        mythicBoosterMultiplier: number = 1,
+    ): Promise<SimulationResult<T>> {
+        return await simulateFromTeams(simType, playerTeam, opponentTeam, mythicBoosterMultiplier);
     },
 };
